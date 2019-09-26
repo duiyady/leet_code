ae37b088d8856bcbee55861631cbbb02a14c148f
duiya <duiyady@163.com>
2019-09-26 13:04:53 +0800

    15完

--- num1_20/15_三数之和.py
+++ num1_20/15_三数之和.py
@@ -9,69 +9,39 @@
 ]
 """
 
+
 def threeSum(nums):
     nums.sort()
-    # print(nums)
-    nums_len = len(nums)
-    result = []
-    if nums_len < 3:
-        return result
-    if nums[0] == nums[1] == nums[2] == 0:
-        return [[0, 0, 0]]
-    if nums[0] >= 0 or nums[nums_len - 1] <= 0:
-        return result
-
-    index = 0
-    last_value = None
-    while index < nums_len-2 and nums[index] <=0:
-        # print("============================================")
-        # print("第一步选择:", nums[index])
-
-        if last_value == None or nums[index] != last_value:
-            index2 = nums_len - 1
-            flag = 0
-            while index2 > index + 1 and flag == 0:
-                sum_2 = nums[index] + nums[index2]
-                # print("第二步选择：", nums[index2], "和为：", sum_2)
-                if sum_2 >= 0 and sum_2 + nums[index] > 0:
-                    # print("if1")
-                    index2 = index2 - 1
-                elif sum_2 >= 0 and sum_2 + nums[index] <= 0:
-                    # print("if2")
-                    # 从负数找
-                    index3 = index + 1
-                    while index3 < index2 and nums[index3] <= 0:
-                        if nums[index3] + sum_2 == 0:
-                            tmp_result = [nums[index], nums[index3], nums[index2]]
-                            result.append(tmp_result)
-                            flag = 1
-                            break
-                        if nums[index3] + sum_2 > 0:
-                            break
-                        index3 = index3 + 1
-                    index2 = index2 - 1
-                elif sum_2 < 0 and sum_2 + nums[index2] < 0:
-                    # print("if3")
-                    break
-                elif sum_2 < 0 and sum_2 + nums[index2] >= 0:
-                    # print("if4")
-                    # 从正数找
-                    index3 = index2 - 1
-                    while index3 > index and nums[index3] >= 0:
-                        if nums[index3] + sum_2 == 0:
-                            tmp_result = [nums[index], nums[index3], nums[index2]]
-                            result.append(tmp_result)
-                            flag = 1
-                            break
-                        if nums[index3] + sum_2 < 0:
-                            break
-                        index3 = index3 - 1
-                    index2 = index2 - 1
-        last_value == nums[index]
-        index = index + 1
-
-    return result
+    res, k = [], 0
+    for k in range(len(nums) - 2):
+        if nums[k] > 0:
+            break
+        if k > 0 and nums[k] == nums[k - 1]:
+            continue
+        i, j = k + 1, len(nums) - 1
+        while i < j:
+            s = nums[k] + nums[i] + nums[j]
+            if s < 0:
+                i += 1
+                while i < j and nums[i] == nums[i - 1]:
+                    i += 1
+            elif s > 0:
+                j -= 1
+                while i < j and nums[j] == nums[j + 1]:
+                    j -= 1
+            else:
+                res.append([nums[k], nums[i], nums[j]])
+                i += 1
+                j -= 1
+                while i < j and nums[i] == nums[i - 1]:
+                    i += 1
+                while i < j and nums[j] == nums[j + 1]:
+                    j -= 1
+    return res
 
 
 if __name__ == '__main__':
-    print(threeSum([-1, 0, 1, 2, -1, -4]))
+    # print(threeSum([1, -1, -1, 1]))
+    # print(threeSum([-1, 0, 1, 2, -1, -4]))
+    # print(threeSum([-1, 0, 1, 0]))
+    print(threeSum([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6]))
