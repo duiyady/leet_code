896bce21f590db7d388134d71223fdc1666e07d0
duiya <duiyady@163.com>
2020-07-22 11:19:45 +0800

    81_commit

---
+++ num001_100/81_搜索旋转排序数组II.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+# @Time: 2020/7/20 22:17
+# @Author: duiya duiyady@163.com
+
+
+"""
+假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+( 例如，数组 [0,0,1,2,2,5,6] 可能变为 [2,5,6,0,0,1,2] )。
+编写一个函数来判断给定的目标值是否存在于数组中。若存在返回 true，否则返回 false。
+示例 1:
+输入: nums = [2,5,6,0,0,1,2], target = 0
+输出: true
+示例 2:
+输入: nums = [2,5,6,0,0,1,2], target = 3
+输出: false
+进阶:
+这是 搜索旋转排序数组 的延伸题目，本题中的 nums  可能包含重复元素。
+这会影响到程序的时间复杂度吗？会有怎样的影响，为什么？
+"""
+
+
+def search(nums, target):
+    start, end = 0, len(nums)-1
+    while start <= end:
+        mid = (start+end)//2
+        if nums[mid] == target:
+            return True
+        if nums[mid] == nums[start]:
+            start += 1
+        elif nums[mid] > nums[start]:
+            # 左边是有序的
+            if target < nums[mid] and target >= nums[start]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        else:
+            # 右边是有序的
+            if target > nums[mid] and target <= nums[end]:
+                start = mid + 1
+            else:
+                end = mid - 1
+    return False
+
+
+
+if __name__ == '__main__':
+    print(search(nums=[2,5,6,0,0,1,2], target=0))
