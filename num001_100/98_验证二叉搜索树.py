6c07c2768e88fe715a2682bc82f92ce4578853a2
duiya <duiyady@163.com>
2020-07-29 18:35:09 +0800

    98_commit

---
+++ num001_100/98_验证二叉搜索树.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-
+# @Time: 2020/7/29 16:38
+# @Author: duiya duiyady@163.com
+
+
+"""
+给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+假设一个二叉搜索树具有如下特征：
+节点的左子树只包含小于当前节点的数。
+节点的右子树只包含大于当前节点的数。
+所有左子树和右子树自身必须也是二叉搜索树。
+示例 1:
+输入:
+    2
+   / \
+  1   3
+输出: true
+示例 2:
+输入:
+    5
+   / \
+  1   4
+     / \
+    3   6
+输出: false
+解释: 输入为: [5,1,4,null,null,3,6]。
+     根节点的值为 5 ，但是其右子节点值为 4 。
+"""
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def isValidBST(root):
+    if root is None:
+        return True
+    result = [-9999999999, ]
+    stack = [[root, 0], ]
+    while stack:
+        tmp = stack[-1]
+        now = len(stack)
+        if tmp[1] == 0:
+            tmp[1] = 1
+            if tmp[0].left is not None:
+                stack.insert(now, [tmp[0].left, 0])
+            if tmp[0].right is not None:
+                stack.insert(now - 1, [tmp[0].right, 0])
+        else:
+            stack.pop()
+            if tmp[0].val <= result[-1]:
+                return False
+            result.append(tmp[0].val)
+    return True
+
